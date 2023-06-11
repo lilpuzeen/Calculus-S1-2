126b3e7bb7e7f1d1b1fcb005902be9a4b2cdec56
armantovmasyan <arman-nn04@mail.ru>
2023-06-11 19:39:53 +0300

    New push

--- LinAlg/eigen.py
+++ LinAlg/eigen.py
@@ -1,16 +1,8 @@
 import numpy as np
-from numpy.typing import NDArray
+import scipy.linalg as sp
+from Stringify import to_string
+A = np.array([[5, 2, -1, 0, 0], [0, 4, 1, 0, 0], [0, -1, 7, 1, 0], [0, 1, -2, 4, 0], [0, -1, 1, 0, 5]])
 
+print(to_string(2*A + 0.5 * sp.expm(A * 0.5)))
 
-def mult(A: NDArray, B: NDArray):
-    # A - row, B - matrix (2,0)
-    C_ilt = np.array([[[10, -2, -6], [-5, 1, 3], [10, -2, -6]], [[10, -10, -10], [-5, 5, 5], [10, -10, -10]], [[-2, -2, -4], [1, 1, 2], [-2, -2, -4]]])
-    result = np.zeros_like(C_ilt)
-    for k in range(3):
-        for i in range(3):
-            for j in range(3):
-                result[k][i][j] = A[i] * B[j][k]
-    return result
-
-
-print(mult(np.array([-2, 1, -2]), np.array([[-5, -5, 1], [1, 5, 1], [3, 5, 2]])))
+# print(mult(np.array([-2, 1, -2]), np.array([[-5, -5, 1], [1, 5, 1], [3, 5, 2]])))
